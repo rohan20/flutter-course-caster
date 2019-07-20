3d1be5efa1f85ad1fac75c751778504320eab227
Rohan Taneja <rohan.bvcoe@gmail.com>
2019-07-21 01:26:46 +0200

    end of lesson: stateless widget

--- first_project/lib/main.dart
+++ first_project/lib/main.dart
@@ -1,7 +1,13 @@
-//import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 void main() {
-//  runApp(FlutterLogo());
-  runApp(CupertinoActivityIndicator());
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FlutterLogo();
+  }
+
 }
