d5c9bb792aefc541445f9b91bca55a493ed7a3c1
Rohan Taneja <rohan.bvcoe@gmail.com>
2019-07-21 03:57:02 +0200

    end of lesson: the MaterialApp widget

--- first_project/lib/main.dart
+++ first_project/lib/main.dart
@@ -7,7 +7,6 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return FlutterLogo();
+    return MaterialApp(home: FlutterLogo());
   }
-
 }
