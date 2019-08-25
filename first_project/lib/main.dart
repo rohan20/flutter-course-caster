97ac7e0481dd3cfa83b2596147e90d74da2472a3
Rohan Taneja <rohan.bvcoe@gmail.com>
2019-08-25 18:38:10 +0200

    end of lesson: styling a text widget

--- first_project/lib/main.dart
+++ first_project/lib/main.dart
@@ -1,9 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
-//  runApp(MyCupertinoApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,19 +9,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(child: Text("Hello World!")),
-      ),
-    );
-  }
-}
-
-class MyCupertinoApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        body: Center(child: CupertinoActivityIndicator()),
+        body: Center(
+          child: Text(
+            "0",
+            style: Theme.of(context).textTheme.display1,
+          ),
+        ),
       ),
     );
   }
