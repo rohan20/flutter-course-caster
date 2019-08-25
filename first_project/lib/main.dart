511df82d8eabeea85db4c59acf82df14bc0bcbcf
Rohan Taneja <rohan.bvcoe@gmail.com>
2019-08-25 11:27:09 +0200

    end of lesson: hot reload inside the main method

--- first_project/lib/main.dart
+++ first_project/lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
+//  runApp(MyCupertinoApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -9,7 +11,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(child: Text("Hello Flutter!")),
+        body: Center(child: Text("Hello World!")),
+      ),
+    );
+  }
+}
+
+class MyCupertinoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: Center(child: CupertinoActivityIndicator()),
       ),
     );
   }
