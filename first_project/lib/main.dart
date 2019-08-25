e58ff88a4272ceb3fa0af651501c36a243f910eb
Rohan Taneja <rohan.bvcoe@gmail.com>
2019-08-25 11:08:38 +0200

    end of lesson: hot reload

--- first_project/lib/main.dart
+++ first_project/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(child: Text("\n\n\nHello Flutter!")),
+        body: Center(child: Text("Hello Flutter!")),
       ),
     );
   }
