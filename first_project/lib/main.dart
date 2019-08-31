6c1527fd95a02d9b5940a7c19d34543a8d87baf2
Rohan Taneja <rohan.bvcoe@gmail.com>
2019-08-31 18:10:36 +0200

    end of lesson: the appbar widget

--- first_project/lib/main.dart
+++ first_project/lib/main.dart
@@ -8,7 +8,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Flutter Demo",
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+      ),
       home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter Demo Home Page"),
+        ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
