45cd7b748d7a7fa3309d3da98113f8d550f0aec7
Rohan Taneja <rohan.bvcoe@gmail.com>
2019-09-01 21:18:58 +0200

    end of lesson: stateful widget

--- first_project/lib/main.dart
+++ first_project/lib/main.dart
@@ -12,29 +12,43 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () {
-            print("FAB pressed");
-          },
-        ),
-        appBar: AppBar(
-          title: Text("Flutter Demo Home Page"),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                "You've pushed this button these many times:",
-              ),
-              Text(
-                "0",
-                style: Theme.of(context).textTheme.display1,
-              ),
-            ],
-          ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyHomePageState();
+  }
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          print("FAB pressed");
+        },
+      ),
+      appBar: AppBar(
+        title: Text("Flutter Demo Home Page"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "You've pushed this button these many times:",
+            ),
+            Text(
+              "0",
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
         ),
       ),
     );
