15f4f4ff3bcd8659c9dc7f68a2b6f9d12f945a2f
Rohan Taneja <rohan.bvcoe@gmail.com>
2019-12-21 20:02:07 +0100

    end of lesson: diving deeper into Columns and Rows

--- more_widgets/lib/main.dart
+++ more_widgets/lib/main.dart
@@ -12,24 +12,26 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("My Home Page"),
         ),
-        body: Center(
-          child: Container(
-            padding: EdgeInsets.all(12),
-            margin: EdgeInsets.all(24),
-            alignment: Alignment.center,
-            constraints: BoxConstraints.expand(height: 300),
-            decoration: BoxDecoration(
-              color: Colors.redAccent,
-              border: Border.all(width: 4, color: Colors.blue),
-              borderRadius: BorderRadius.circular(16),
-            ),
-            child: Container(
-              color: Colors.yellow,
-              child: Text(
-                "This is a text widget. This widget is inside a Container widget.",
-                textAlign: TextAlign.center,
+        body: Container(
+          color: Colors.lightBlue,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Container(
+                height: 50,
+                color: Colors.green,
+                child: Text("Columns"),
               ),
-            ),
+              Container(
+                color: Colors.orange,
+                child: Text("and"),
+              ),
+              Container(
+                color: Colors.red,
+                child: Text("Rows"),
+              ),
+            ],
           ),
         ),
       ),
