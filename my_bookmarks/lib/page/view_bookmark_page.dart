74e5954d52945f455d734247f4add1dfed7d5b54
Rohan Taneja <rohan.bvcoe@gmail.com>
2020-01-27 04:09:14 +0100

    end of lesson: integrating a flutter plugin - webview

--- my_bookmarks/lib/page/view_bookmark_page.dart
+++ my_bookmarks/lib/page/view_bookmark_page.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class ViewBookmarkPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("View bookmark")),
+      body: WebView(initialUrl: "https://google.com"),
     );
   }
 }
