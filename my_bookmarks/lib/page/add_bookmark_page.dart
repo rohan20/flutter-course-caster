54ed7648df85ff5f4ae8aa769e2aea485335faae
Rohan Taneja <rohan.bvcoe@gmail.com>
2020-02-01 17:48:59 +0100

    end of lesson: saving a new bookmark

--- my_bookmarks/lib/page/add_bookmark_page.dart
+++ my_bookmarks/lib/page/add_bookmark_page.dart
@@ -1,10 +1,68 @@
 import 'package:flutter/material.dart';
 
-class AddBookmarkPage extends StatelessWidget {
+class AddBookmarkPage extends StatefulWidget {
+
+  @override
+  _AddBookmarkPageState createState() => _AddBookmarkPageState();
+}
+
+class _AddBookmarkPageState extends State<AddBookmarkPage> {
+  final _titleTextController = TextEditingController();
+
+  final _linkTextController = TextEditingController();
+
+  final _linkFocusNode = FocusNode();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Add a new bookmark")),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.check),
+        backgroundColor: Colors.green,
+        onPressed: () {
+          print("Title: ${_titleTextController.text}");
+          print("Link: ${_linkTextController.text}");
+        },
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              autofocus: true,
+              controller: _titleTextController,
+              textInputAction: TextInputAction.next,
+              onFieldSubmitted: (textInput) {
+                FocusScope.of(context).requestFocus(_linkFocusNode);
+              },
+              decoration: InputDecoration(
+                  icon: Icon(Icons.title),
+                  labelText: "Title",
+                  hintText: "Title of the bookmark",
+                  border: OutlineInputBorder()),
+            ),
+            SizedBox(height: 16.0),
+            TextFormField(
+              controller: _linkTextController,
+              focusNode: _linkFocusNode,
+              decoration: InputDecoration(
+                  icon: Icon(Icons.link),
+                  labelText: "URL",
+                  hintText: "Webpage link",
+                  border: OutlineInputBorder()),
+            )
+          ],
+        ),
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    _titleTextController.dispose();
+    _linkTextController.dispose();
+    _linkFocusNode.dispose();
+    super.dispose();
   }
 }
