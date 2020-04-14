db2dd0792d5bc00a69fef991e4e4c485c7594286
Rohan Taneja <rohan20@users.noreply.github.com>
2020-04-14 02:44:29 +0200

    add live template "stless_with_appbar"

---
+++ live_template_stless_with_appbar.dart
@@ -0,0 +1,9 @@
+class $NAME$ extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("$TITLE$")),
+    );
+  }
+}
+$END$
